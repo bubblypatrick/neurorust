558c5b2955f10bbc128420c9e94182bd7f751e79
Patrick Champagne <patrickrenechampagne@gmail.com>
2025-07-07 10:30:38 -0700

    setup axum hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+  routing::get,
+  Router,
+};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+  let app: Router = Router::new()
+    .route("/", get(|| async {
+      "Hello, world!"}
+    ));
+
+  let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+  axum::serve(listener, app).await.unwrap();
 }
